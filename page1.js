3c60f77f56ae6e099f18880b5dfafe41944ec243
aklimaakter20 <aklimaakter20@gmail.com>
2024-08-28 23:15:49 +0600

    first commit

---
+++ page1.js
@@ -0,0 +1,91 @@
+// Number-1
+
+ let carName = "volvo";
+ console.log(carName)
+
+{// Number-2
+
+    let firstName = "John";  
+    let lastName = "Doe" 
+    let age = 35 ; 
+    firstName = "John", lastName = "Doe", age = 35;
+    console.log(age);
+}
+
+
+// Ans no -3
+
+{let x= 10;
+    y = 5;
+     x = x * y;
+   console.log(x)     // 50 is a value of x
+   }
+ 
+
+// Ans no -4
+{
+let length = 16; //number data type
+ let lastName = "johnson"; //string data type
+ const x = {
+     firstName: "john", //string data type
+    lastName : "Doe"//string data type
+ }
+ };
+
+// Ans no -5
+
+
+ function myFunction(){
+    alert("Hello World!");
+ }
+ myFunction();  //function call and output is Hello World!
+
+// Ans no -6
+
+ const person = {
+     name : "john",
+     age : 50
+ };
+ alert(person.name + " is " + person.age);
+
+// Ans no -7
+<button type="button" onclick= "alert('Try to fix it!')">Click Me</button> 
+ 
+
+// Ans no -8
+ const cars = ["Volvo", "Jeep", "Mercedes"];
+ console.log(cars.length)
+
+ const Brand = ["Volvo", "Jeep", "Mercedes"];
+ Brand[0] = "Ford"
+  console.log(Brand)
+{
+// Ans no -9
+
+ let x = Math.random();
+ console.log(x)
+}
+{
+ let x = Math.max(10,20);
+ console.log(x)
+
+}
+
+ let a = Math.sqrt(9);
+ console.log(a)
+
+// Ans no -10 -a
+ 
+{  var x = 10;
+    var y = 5;
+    var z = x > 5;
+    alert(z)
+}
+
+{
+    //10- b.
+    var age = 15
+     let personAge = (age < 18) ? "Too young":"Old enough";
+     console.log(personAge) 
+    }
+ 
---
+++ page2.js
@@ -0,0 +1,86 @@
+
+
+// 1 no ans 
+{ 
+  function celsiusToFahrenheit(celsius) {
+  let Fahrenheit = (celsius * 9/5) + 32;
+  return Fahrenheit;
+}
+
+    console.log(celsiusToFahrenheit(0)); // 32
+    console.log(celsiusToFahrenheit(25));// 77
+  }
+  
+// ans no 2 
+{ function isEven(num){
+  return num % 2 === 0;
+}
+console.log(isEven(4));
+console.log(isEven(7));
+}
+
+// 3 no ans ok
+ function sum(a, b) {
+    return a + b;
+   }
+  
+  console.log(sum(3, 4)); // 7
+   console.log(sum(10, 20)); // 30
+
+// 4 no ans
+
+function findSmallestNum(arr) {
+    return (Math.min(arr));
+  }
+  
+  console.log(Math.min([3, 5, 1, 9])); // 1
+  console.log(Math.min([-1, -5, 0, 10])); // -5
+  
+
+ {
+  // 9 no ans 
+ function reverseString(str){
+  return str.split('').reverse().join("");
+}
+console.log(reverseString("hello"));
+console.log(reverseString("world"));
+} 
+
+
+//   10 no ans
+// function toLowerCase(str) {
+//     return (text.toLowerCase);
+//   }
+  
+//   console.log(toLowerCase("HELLO WORLD")); // "hello world"
+//   console.log(toLowerCase("JavaScript")); // "javascript"
+
+//11 no ans 
+ {    
+    function stringLength(str) {
+       return str.length;
+     }
+    
+     console.log(stringLength("hello")); // 5
+     console.log(stringLength("world")); // 5}
+ }
+
+// 13 no ans
+
+
+
+ function getLastElement(arr) {
+     return(getLastElement[arr.length - 1]);
+   }
+  
+  console.log(getLastElement([1, 2, 3])) 
+   console.log(getLastElement(["a", "b", "c"])) 
+
+//    14 no ans ok
+     function getFirstCharacter(str) {
+     return str.charAt(0);
+   }
+  
+  console.log(getFirstCharacter("hello")); // "h"
+  console.log(getFirstCharacter("world")); // "w"
+  
